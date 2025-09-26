582e7b03ea0dd9f88a98dfa71cad113a5a57e304
Rodrigo Dantas <rodrigo6031@gmail.com>
2025-09-26 20:09:36 -0300

    Add UserDTO

--- src/main/java/com/rjdo/springmongodb/controllers/UserController.java
+++ src/main/java/com/rjdo/springmongodb/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.rjdo.springmongodb.controllers;
 
+import com.rjdo.springmongodb.dto.UserDTO;
 import com.rjdo.springmongodb.entities.User;
 import com.rjdo.springmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserController {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDto);
     }
 }
