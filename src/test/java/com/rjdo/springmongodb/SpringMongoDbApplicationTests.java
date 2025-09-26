624d9bff40592902b05bbf8fa9dfc94ad3bdc8c2
Rodrigo Dantas <rodrigo6031@gmail.com>
2025-09-26 15:45:17 -0300

    create project

---
+++ src/test/java/com/rjdo/springmongodb/SpringMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjdo.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
